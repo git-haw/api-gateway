9afeb59a06a6e00529f59fba73855b62cf240f44
haw <haw358linux@163.com>
2018-06-28 17:37:19 +0800

    服务网关 Zuul（路由）【Finchley 版】

---
+++ src/main/java/top/haw/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package top.haw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
